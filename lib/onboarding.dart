9a27c96d1410907464dfd60ece726878f7d1780e
Serdyuk-prog <tony.not.gloomy@gmail.com>
2023-03-05 16:54:53 +0700

    main stuff is done

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:words_factory/content_modal.dart';
+import 'package:words_factory/home.dart';
 
 class Onboarding extends StatefulWidget {
   const Onboarding({super.key});
@@ -9,6 +10,15 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
+  int currentIndex = 0;
+  final PageController _controller = PageController(initialPage: 0);
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +26,13 @@ class _OnboardingState extends State<Onboarding> {
         children: [
           Expanded(
             child: PageView.builder(
+                controller: _controller,
                 itemCount: contents.length,
+                onPageChanged: (int index) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
                 itemBuilder: (_, i) {
                   return Padding(
                     padding: const EdgeInsets.all(30.0),
@@ -46,12 +62,55 @@ class _OnboardingState extends State<Onboarding> {
                 }),
           ),
           Container(
-            height: 55,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                  contents.length, (index) => buildDot(index, context)),
+            ),
+          ),
+          Container(
+            height: 60,
+            margin: const EdgeInsets.all(40),
             width: double.infinity,
-            color: Theme.of(context).primaryColor,
+            // color: Theme.of(context).primaryColor,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16))),
+              child: Text(
+                currentIndex == contents.length - 1 ? "Let’s Start" : "Next",
+                style: const TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                if (currentIndex == contents.length - 1) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => Home(),
+                    ),
+                  );
+                }
+                _controller.nextPage(
+                  duration: Duration(milliseconds: 100),
+                  curve: Curves.bounceIn,
+                );
+              },
+            ),
           )
         ],
       ),
     );
+  }
+
+  Container buildDot(int index, BuildContext context) {
+    return Container(
+        height: 10,
+        width: currentIndex == index ? 25 : 10,
+        margin: const EdgeInsets.only(right: 5),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Theme.of(context).primaryColor,
+        ));
   }
 }
