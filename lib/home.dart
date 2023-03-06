8346cb7ffdf5813db77d532a7d17e3e91442e536
Serdyuk-prog <tony.not.gloomy@gmail.com>
2023-03-06 14:21:29 +0700

    nav - done

--- lib/home.dart
+++ lib/home.dart
@@ -10,24 +10,55 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
+    const List<Widget> pages = <Widget>[
+      Icon(
+        Icons.book,
+        size: 150,
+      ),
+      Icon(
+        Icons.web_stories,
+        size: 150,
+      ),
+      Icon(
+        Icons.play_circle,
+        size: 150,
+      ),
+    ];
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.call),
-            label: 'Calls',
+      body: Center(
+        child: pages.elementAt(_selectedIndex), //New
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+            border: Border.all(color: Colors.grey)),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
-            label: 'Camera',
+          child: BottomNavigationBar(
+            selectedItemColor: Theme.of(context).primaryColor,
+            unselectedItemColor: Colors.grey[400],
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.book),
+                label: 'Dictionary',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.web_stories),
+                label: 'Training',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.play_circle),
+                label: 'Video',
+              ),
+            ],
+            currentIndex: _selectedIndex, //New
+            onTap: _onItemTapped,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat),
-            label: 'Chats',
-          ),
-        ],
-        currentIndex: _selectedIndex, //New
-        onTap: _onItemTapped,
+        ),
       ),
     );
   }
