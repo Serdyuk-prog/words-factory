9d6dbe5d1d742f0aade4a6f28570ac1b18386917
Serdyuk-prog <tony.not.gloomy@gmail.com>
2023-03-06 17:17:54 +0700

    done

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -12,10 +12,17 @@
 @import flutter_native_splash;
 #endif
 
+#if __has_include(<webview_flutter_wkwebview/FLTWebViewFlutterPlugin.h>)
+#import <webview_flutter_wkwebview/FLTWebViewFlutterPlugin.h>
+#else
+@import webview_flutter_wkwebview;
+#endif
+
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
   [FlutterNativeSplashPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterNativeSplashPlugin"]];
+  [FLTWebViewFlutterPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTWebViewFlutterPlugin"]];
 }
 
 @end
--- lib/pages/video.dart
+++ lib/pages/video.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class Video extends StatelessWidget {
+class Video extends StatefulWidget {
   const Video(BuildContext context, {super.key});
 
   @override
+  State<Video> createState() => _VideoState();
+}
+
+class _VideoState extends State<Video> {
+  late final WebViewController controller;
+  String url =
+      'https://learnenglish.britishcouncil.org/general-english/video-zone';
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (navigation) {
+            if (navigation.url != url) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse(url),
+      );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Icon(
-      Icons.play_circle,
-      size: 150,
+    return Scaffold(
+      body: WebViewWidget(
+        controller: controller,
+      ),
     );
   }
 }
