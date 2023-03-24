8118aea115a886a00ce62da00872ac0f7cd18a91
Serdyuk-prog <tony.not.gloomy@gmail.com>
2023-03-24 18:00:22 +0700

    some progress

--- lib/pages/dictionary.dart
+++ lib/pages/dictionary.dart
@@ -1,21 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
-class Dictionary extends StatelessWidget {
+class Dictionary extends StatefulWidget {
   const Dictionary(BuildContext context, {super.key});
+
+  @override
+  State<Dictionary> createState() => _DictionaryState();
+}
+
+class _DictionaryState extends State<Dictionary> {
+  final TextEditingController _controller = TextEditingController();
+  String _textFieldValue = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 30),
+      // resizeToAvoidBottomInset: false,
+      body: Stack(children: [
+        Align(
+          alignment: Alignment.topCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 30),
             child: TextField(
-              style: TextStyle(
+              controller: _controller,
+              style: const TextStyle(
                   fontSize: 14, fontWeight: FontWeight.w400, height: 1),
               decoration: InputDecoration(
-                  border: OutlineInputBorder(
+                  border: const OutlineInputBorder(
                     borderRadius: BorderRadius.all(
                       Radius.circular(16.0),
                     ),
@@ -23,77 +35,100 @@ class Dictionary extends StatelessWidget {
                   hintText: 'Cooking',
                   suffixIcon: IconButton(
                       color: Colors.black,
-                      onPressed: null,
-                      icon: Icon(Icons.search))),
+                      onPressed: () {
+                        setState(() {
+                          _textFieldValue = _controller.text;
+                        });
+                        fetchData(_textFieldValue);
+                      },
+                      icon: const Icon(Icons.search))),
+              onSubmitted: (_) {
+                setState(() {
+                  _textFieldValue = _controller.text;
+                });
+                fetchData(_textFieldValue);
+              },
             ),
           ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                children: [
-                  Row(
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 90),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
                     children: [
-                      const Text(
-                        "Cooking",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: Text(
-                          "[ˈkʊkɪŋ]",
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 17
-                          ),
-                        ),
-                      ),
-                      Icon(
-                        Icons.volume_up,
-                        size: 30,
-                        color: Theme.of(context).primaryColor,
-                      )
-                    ],
-                  ),
-                  Row(
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.only(right: 20, bottom: 20, top: 20),
-                        child: Text(
-                          "Part of Speech:",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w600),
-                        ),
-                      ),
-                      Text(
-                        "Noun",
-                        style: TextStyle(fontSize: 17),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.only(right: 20, bottom: 20),
-                        child: Text("Meanings:",
+                      Row(
+                        children: [
+                          const Text(
+                            "Cooking",
                             style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w600)),
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            child: Text(
+                              "[ˈkʊkɪŋ]",
+                              style: TextStyle(
+                                  color: Theme.of(context).primaryColor,
+                                  fontSize: 17),
+                            ),
+                          ),
+                          Icon(
+                            Icons.volume_up,
+                            size: 30,
+                            color: Theme.of(context).primaryColor,
+                          )
+                        ],
                       ),
+                      Row(
+                        children: const [
+                          Padding(
+                            padding:
+                                EdgeInsets.only(right: 20, bottom: 20, top: 20),
+                            child: Text(
+                              "Part of Speech:",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                          Text(
+                            "Noun",
+                            style: TextStyle(fontSize: 17),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: const [
+                          Padding(
+                            padding: EdgeInsets.only(right: 20, bottom: 20),
+                            child: Text("Meanings:",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w600)),
+                          ),
+                        ],
+                      ),
+                      const Meaning(),
+                      const Meaning(),
+                      const Meaning(),
+                      const Meaning(),
+                      const Meaning(),
                     ],
                   ),
-                  const Meaning(),
-                  const Meaning(),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-
-        Container(
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
             height: 60,
-            margin: const EdgeInsets.only(right: 30, left: 30, bottom: 20),
+            margin: const EdgeInsets.only(right: 30, left: 30, bottom: 10),
             width: double.infinity,
             child: TextButton(
               style: TextButton.styleFrom(
@@ -106,11 +141,22 @@ class Dictionary extends StatelessWidget {
               ),
               onPressed: () {},
             ),
-          )
-
-        ],
-      ),
+          ),
+        )
+      ]),
     );
+  }
+
+  Future<void> fetchData(String urlEndpoint) async {
+    final apiUrl = Uri.parse(
+        'https://api.dictionaryapi.dev/api/v2/entries/en/$urlEndpoint');
+    final response = await http.get(apiUrl);
+    if (response.statusCode == 200) {
+      final jsonData = jsonDecode(response.body);
+      print(jsonData[0]['phonetics'][0]['text']);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
   }
 }
 
@@ -154,3 +200,17 @@ class Meaning extends StatelessWidget {
     );
   }
 }
+
+// class WordData {
+//   String word = "";
+//   String phonetic = "";
+
+//   Person({required this.name, required this.age});
+
+//   factory Person.fromJson(Map<String, dynamic> json) {
+//     return Person(
+//       name: json['name'],
+//       age: json['age'],
+//     );
+//   }
+// }
